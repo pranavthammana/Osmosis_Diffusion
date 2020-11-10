2e5807c158c4cb717170100a0b073b808eb96e49
pranav-thammana <59235137+195-084@users.noreply.github.com>
2020-11-09 23:40:49 -0800

    Update 1d_osmosis.py

--- 1d_osmosis.py
+++ 1d_osmosis.py
@@ -19,17 +19,18 @@ import matplotlib.mlab as mlab
 
 #Point class setup - basically allows me to get one simulation done
 class Point:
-    def __init__(self, pos):
+    def __init__(self, pos, percent):
         self.pos = pos
         self.pos_track = [pos]
+        self.percent = percent
         
     def pos_update(self):
         num = rand.randint(1, 100)
-        if 1 <= num <= 25:
+        if 1 <= num <= self.percent:
             self.pos = self.pos + 1
             self.pos_track.append(self.pos)
             return 1
-        elif 26 <= num <= 50:
+        elif self.percent+1 <= num <= self.percent*2:
             self.pos = self.pos - 1
             self.pos_track.append(self.pos)
             return -1
@@ -38,60 +39,71 @@ class Point:
 
 
 #This is the looping mechanism per se - does 100 trajectories over 100 time intervals
+d_coeff = {}
 trajectory_compilation = dict() #Will be a dictionary with key being trajectory number and entry being a list with x values
+for percent in range(1,3):
+    for i in range(100):
+        point = Point(0, percent)
+        for j in range(100):
+           point.pos_update()
+        trajectory_compilation['t' + str(i)] = point.pos_track
+    
+    df = pd.DataFrame(data=trajectory_compilation) #data frame from which work will be done from for now
+    
+    plt.plot(df.mean(1))
+    
+    plt.xlabel('time')
+    plt.ylabel('x position')
+    plt.title(f'Mean for {percent}')
+    
 
-for i in range(100):
-    point = Point(0)
-    for j in range(100):
-       point.pos_update()
-    trajectory_compilation['t' + str(i)] = point.pos_track
+    x = []
+    for i in range(101):
+        x.append(i)
+    
+    meansquare = np.square(df).mean(1)
+    
+    m, b = np.polyfit(x, list(meansquare), 1)
+    
+    d_coeff[percent] = m
+    
+    #print(m, percent)
+    
+    
+    plt.show()
+    
+    plt.plot(np.square(df).mean(1))
+    
+    plt.xlabel('time')
+    plt.ylabel('x position')
+    plt.title(f'Mean square for {percent}')
+    
+    plt.show()
+    
+    list_of_x = []
+    for i in trajectory_compilation.keys():
+        for j in trajectory_compilation[i]:
+            list_of_x.append(j)
+    bins = max(list_of_x) - min(list_of_x)
+    
+    (mu, sigma) = norm.fit(list_of_x)
+    
+    y = norm.pdf( bins, mu, sigma)
+    
+    l = plt.plot(bins, y, 'r--', linewidth=2)
 
-df = pd.DataFrame(data=trajectory_compilation) #data frame from which work will be done from for now
+    plt.hist(list_of_x, bins = bins)
+    
+    plt.xlabel('x position at ' + str(percent))
+    plt.ylabel('frequency')
+    plt.title(r'$\mathrm{Histogram\ of\ X Position:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
+    
+    plt.show()
+    
 
-plt.plot(df.mean(1))
+y_vals = []
+for i in d_coeff.keys():
+    y_vals.append(d_coeff[i])
 
-plt.xlabel('time')
-plt.ylabel('x position')
-plt.title('Mean')
+plt.scatter(list(d_coeff.keys()), y_vals)
 
-x = []
-for i in range(101):
-    x.append(i)
-
-meansquare = np.square(df).mean(1)
-
-m, b = np.polyfit(x, list(meansquare), 1)
-
-print(m)
-
-
-plt.show()
-
-plt.plot(np.square(df).mean(1))
-
-plt.xlabel('time')
-plt.ylabel('x position')
-plt.title('Mean square')
-
-plt.show()
-
-list_of_x = []
-for i in trajectory_compilation.keys():
-    for j in trajectory_compilation[i]:
-        list_of_x.append(j)
-bins = max(list_of_x) - min(list_of_x)
-
-(mu, sigma) = norm.fit(list_of_x)
-
-y = norm.pdf( bins, mu, sigma)
-
-l = plt.plot(bins, y, 'r--', linewidth=2)
-
-plt.hist(list_of_x, bins = bins)
-
-plt.xlabel('x position')
-plt.ylabel('frequency')
-plt.title(r'$\mathrm{Histogram\ of\ X Position:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
-
-plt.show()
-#df.to_excel(r'/Users/home/Dropbox/Internship App stuff/pain.xlsx', index = False)
