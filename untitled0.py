43aa5c376c208d533c34f4c4d8d397145f197951
pranavthammana <61713704+pranavthammana@users.noreply.github.com>
2021-01-24 16:04:00 -0800

    Create untitled0.py

---
+++ untitled0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 14:53:09 2021
+
+@author: vtham
+"""
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from scipy.stats import norm
+
+import random as rand
+
+import pandas as pd
+
